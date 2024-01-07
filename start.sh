2f9c0012033c0f3662490885d1b7ed94887e0d40
FilmClan <148469068+FilmClan@users.noreply.github.com>
2024-01-07 16:22:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/FilmClan/FilmClan.git /FilmClan 
+  git clone https://github.com/FilmClan/FilmClan.git /FilmClan
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /FilmClan 
+  git clone $UPSTREAM_REPO /FilmClan
 fi
-cd /FilmClan 
+cd /Lucifer
 pip3 install -U -r requirements.txt
-echo "Starting FilmClan...."
+echo "Starting Bot...."
 python3 bot.py

aee8bac7c02c2ba0c9ee7cafc9282239baa017bc
FilmClan <148469068+FilmClan@users.noreply.github.com>
2024-01-10 16:37:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -463,7 +463,9 @@ async def start(client, message):
         f_caption = f"@FilmClan  {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
     if not await check_verification(client, message.from_user.id) and VERIFY == True:
         btn = [[
-            InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
+            InlineKeyboardButton("⁉️ Vᴇʀɪғʏ ⁉️", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
+            ],[
+            InlineKeyboardButton('⁉️ Hᴏᴡ Tᴏ Vᴇʀɪғʏ ⁉️', url="https://t.me/FilmClan")
         ]]
         await message.reply_text(
             text="<b>You are not verified !\nKindly verify to continue !</b>",

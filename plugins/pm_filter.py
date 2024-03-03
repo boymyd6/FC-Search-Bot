75b1a5defdc9eccf72b7539b129fbd7517bd2376
ᴍᴀɪᴅɪ⋆ᴋɪɴɢ <158250802+Maheshbox@users.noreply.github.com>
2024-03-03 21:00:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1950,7 +1950,7 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+            m=await message.reply_text(f"<b><i>  '{search}' 🔎</i></b>")
             search = search.lower()
             find = search.split(" ")
             search = ""

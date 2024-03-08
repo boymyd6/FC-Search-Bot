c42b1ea8ac08e894653a9522f30452110bc759eb
ᴍᴀɪᴅɪ⋆ᴋɪɴɢ <158250802+Maheshbox@users.noreply.github.com>
2024-03-08 16:45:03 +0530

    Update share_text.py

--- plugins/Extra/share_text.py
+++ plugins/Extra/share_text.py
@@ -21,7 +21,7 @@ async def share_text(client, message):
         await message.reply_text(
             text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates channel", url=f"https://t.me/hd_top_flix")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates channel", url=f"https://t.me/FilmClan")]])
             )                                                   
         return
     await message.reply_text(

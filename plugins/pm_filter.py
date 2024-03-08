c8ce0bd1cb697c789dadd1d0b1b418e913e1a839
ᴍᴀɪᴅɪ⋆ᴋɪɴɢ <158250802+Maheshbox@users.noreply.github.com>
2024-03-08 16:06:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1950,7 +1950,7 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_text(f"<b><i>  '{search}' 🔎</i></b>")
+            m=await message.reply_text(f"<b><i> '{search}' </i></b>")
             search = search.lower()
             find = search.split(" ")
             search = ""
@@ -1979,7 +1979,7 @@ async def auto_filter(client, msg, spoll=False):
     else:
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
-        m=await message.reply_text(f"<b><i>  '{search}' 🔎</i></b>")
+        m=await message.reply_text(f"<b><i> '{search}' </i></b>")
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
     pre = 'filep' if settings['file_secure'] else 'file'

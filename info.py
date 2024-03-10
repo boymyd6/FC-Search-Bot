b68f484657f66802583ed4e49bb5af3eb6e190d2
ᴍᴀɪᴅɪ⋆ᴋɪɴɢ <158250802+Maheshbox@users.noreply.github.com>
2024-03-10 22:49:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -84,7 +84,7 @@ LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin
 
 SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "season 6" , "season 7" , "season 8" , "season 9" , "season 10"]
 
-
+QUALITIES = ["360P", "", "480P", "", "720P", "", "1080P", "", "1440P", "", "2160P", ""]
 
 # Online Stream and Download
 NO_PORT = bool(environ.get('NO_PORT', False))

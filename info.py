2e56943ed39306a32c846c4128ebbea58f7235d2
𓊈ᴍʏᴅ ❀ ʙᴏʏ』࿐ <158250802+MYD-BOY@users.noreply.github.com>
2024-03-15 07:16:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,12 +97,12 @@ else:
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://filmclan-2lem.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://filmclan-2lem.onrender.com".format(FQDN, PORT)
+    "https://filmclan-2lem.onrender.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'MYD-BOY'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'LazyPrincess'))
+name = str(environ.get('name', 'FILMCLAN'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://filmclan-2lem.onrender.com".format(FQDN)
+    URL = "https://filmclan-2lem.onrender.com/".format(FQDN)
 else:
-    URL = "https://filmclan-2lem.onrender.com".format(FQDN)
+    URL = "https://filmclan-2lem.onrender.com/".format(FQDN)
 
 
 

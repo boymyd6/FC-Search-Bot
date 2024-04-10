4196f5586a3418a2e067f456581d9c18d049a495
𓊈ᴍʏᴅ ❀ ʙᴏʏ』࿐ <158250802+MYD-BOY@users.noreply.github.com>
2024-04-10 15:02:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jarramahesh20:QUPAA4JY8qkUou6U@cluster0.pl3b8ro.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "RIONETWORKS")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://filmclan:Xi2JMC7YSXk1YOFw@cluster0.mvjljxf.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "filmclan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others

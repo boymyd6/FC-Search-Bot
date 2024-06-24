677b3cd8d35411a119b3a9a2b3cfa877fca06dee
ᴍʏᴅ⋆ʙᴏʏ࿐ <161321808+MYDBOY@users.noreply.github.com>
2024-06-24 07:52:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "5907438024:AAG6iujoopeBamMDoyJgU9R5KI4XCI4
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/20d78b2034e4fb8f3ef87.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/d7e8e245ede2f176998c1.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", 0)
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")

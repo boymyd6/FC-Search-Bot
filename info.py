402a5db750ca46dabf0d984e1124bded3364dd40
ᴍʏᴅ⋆ʙᴏʏ࿐ <161321808+MYDBOY@users.noreply.github.com>
2024-06-29 18:18:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "autofcbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'urlshortx.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '7bbbfec870fabf11b3ce6b45b391865d6f2eaca6')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
@@ -96,8 +96,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://autofilm3.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://autofilm3.onrender.com/".format(FQDN, PORT)
+URL = "https://autofilmbot-1.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://autofilmbot-1.onrender.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'MYD-BOY'))
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://autofilm3.onrender.com/".format(FQDN)
+    URL = "https://autofilmbot-1.onrender.com/".format(FQDN)
 else:
-    URL = "https://autofilm3.onrender.com/".format(FQDN)
+    URL = "https://autofilmbot-1.onrender.com/".format(FQDN)
 
 
 

6d361a59e429c38ee0fb701b3fbca0dced3209eb
ᴍʏᴅ⋆ʙᴏʏ࿐ <161321808+MYDBOY@users.noreply.github.com>
2024-07-07 12:24:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '25065882'))
 API_HASH = environ.get('API_HASH', '7711af532d45686e38c6b360161e2483')
-BOT_TOKEN = environ.get('BOT_TOKEN', "5907438024:AAG6iujoopeBamMDoyJgU9R5KI4XCI4NNxk")
+BOT_TOKEN = environ.get('BOT_TOKEN', "5907438024:AAG-waNJ671BnGXvYFJDVWwbTRRAVGXPhog")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -96,8 +96,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://autofilmbot-vwpi.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://autofilmbot-vwpi.onrender.com/".format(FQDN, PORT)
+URL = "https://autofilmbot-4n0q.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://autofilmbot-4n0q.onrender.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'MYD-BOY'))
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://autofilmbot-vwpi.onrender.com/".format(FQDN)
+    URL = "https://autofilmbot-4n0q.onrender.com/".format(FQDN)
 else:
-    URL = "https://autofilmbot-vwpi.onrender.com/".format(FQDN)
+    URL = "https://autofilmbot-4n0q.onrender.com/".format(FQDN)
 
 
 

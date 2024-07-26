364477882a3eac9bf32636106cb6cb63cfcbd91e
FILMCLAN <jarramahesh24@gmail.com>
2024-07-26 08:47:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MYDBOY/AutoFilmBot.git /AutoFilmBot
+  git clone https://github.com/FILMCLAN/FC-Search-Bot.git /FC-Search-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilmBot
+  git clone $UPSTREAM_REPO /FC-Search-Bot
 fi
 cd /AutoFilmBot
 pip3 install -U -r requirements.txt
-echo "Starting AutoFilmBot...."
+echo "Starting FC-Search-Bot...."
 python3 bot.py

5bc52f030cb74125227ff24a35cd02c43aa910e1
FILMCLAN <jarramahesh24@gmail.com>
2024-09-15 17:53:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '25065882'))
 API_HASH = environ.get('API_HASH', '7711af532d45686e38c6b360161e2483')
-BOT_TOKEN = environ.get('BOT_TOKEN', "5907438024:AAG-waNJ671BnGXvYFJDVWwbTRRAVGXPhog")
+BOT_TOKEN = environ.get('BOT_TOKEN', "5907438024:AAEmKKH0PBHwkRtuHQyo4BNdfpJUjlZkqWU")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -98,8 +98,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://autofilmbot-hepe.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://autofilmbot-hepe.onrender.com/".format(FQDN, PORT)
+URL = "https://upset-shawna-perimey33-c5b647de.koyeb.app".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://upset-shawna-perimey33-c5b647de.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'MYD-BOY'))
@@ -114,9 +114,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://autofilmbot-hepe.onrender.com/".format(FQDN)
+    URL = "https://upset-shawna-perimey33-c5b647de.koyeb.app/".format(FQDN)
 else:
-    URL = "https://autofilmbot-hepe.onrender.com/".format(FQDN)
+    URL = "https://upset-shawna-perimey33-c5b647de.koyeb.app/".format(FQDN)
 
 
 
